54e27ddfda31c11458df758180bf66b778fae784
zhuofanw <zhuofanw@sfu.ca>
2020-12-15 00:10:43 +0800

    using postcss-loader deal with browser incompatibility issue & setup development as default NODE_ENV

---
+++ 10.css兼容性处理/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+//设置nodejs坏境变量
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+    entry:'./src/js/index.js',
+    output: {
+        filename:'js/built.js',
+        path:resolve(__dirname,'build')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                   {
+                       loader:'postcss-loader',
+                       options:{
+                           postcssOptions:{
+                               plugins:['postcss-preset-env']
+                           }
+                       }
+                   }
+                ]
+            }
+        ]
+     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'css/built.css'
+        })
+    ],
+    mode:'development',
+};
