7b0611b760e60a6e51538b19f3683d669614011a
zhuofanw <zhuofanw@sfu.ca>
2020-12-15 19:20:56 +0800

    write full webpack.config.js in production mode

---
+++ 16.生产环境配置/src/js/index.js
@@ -0,0 +1,13 @@
+// import "@babel/polyfill";
+
+const add = (x, y) => x + y;
+
+// eslint-disable-next-line
+new Promise((resolve) => {
+	setTimeout(() => {
+		console.log("定时器执行完毕");
+		resolve();
+	}, 1000);
+});
+
+console.log(add(2, 5));
